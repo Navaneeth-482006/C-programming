72a1b44bed229299c617a9d2a063252d52d5dc21
Navaneeth-482006 <navaneethdinfo@gmail.com>
2026-04-23 21:45:44 +0530

    Add files via upload

---
+++ Range.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int a;
+  printf("enter the A value \n");
+  scanf("%d",&a);
+  int res=(a>=3)&&(a<=13);
+  printf("%d is in the range of 3-13 i.e is:%d",a,res);
+   return 0;
+}
